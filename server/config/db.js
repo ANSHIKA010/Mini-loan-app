cf4ca5098dc0f555b98d1caa299d34984edcf8a5
ANSHIKA010 <sanshikas010@gmail.com>
2024-10-29 03:13:58 +0530

    db connection established

---
+++ server/config/db.js
@@ -0,0 +1,17 @@
+import { connect } from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        await connect(process.env.MONGO_URI);
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
+
