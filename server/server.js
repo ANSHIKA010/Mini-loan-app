368cde7af20624cd82ec20d6f6e0e515b9f05e66
ANSHIKA010 <sanshikas010@gmail.com>
2024-10-29 19:40:23 +0530

    added cors config

--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import loanRoutes from './routes/loanRoutes.js';
+import cors from 'cors';
 
 dotenv.config();
 
+
 const app = express();
 connectDB();
+app.use(cors());
 
 app.use(express.json());
 app.use('/api/users', userRoutes);     // User-related routes
